e0bea8ab7c613c81ef96f9ad18e3e028b0794c32
Sagar G C <sagar@tulipstechnologies.com>
2025-03-18 11:46:34 +0545

    Fix the issues with Chakra menu.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Button, ButtonGroup, Grid, GridItem, HStack, Show } from '@chakra-ui/react'
+import { Button, ButtonGroup, Grid, GridItem, HStack, Flex, Show, Box } from '@chakra-ui/react'
 import NavBar from './components/NavBar';
 import GameGrid from './components/GameGrid';
 import GenreList from './components/GenreList';
@@ -46,10 +46,14 @@ function App() {
       </Show>
 
       <GridItem gridArea="main">
-        <HStack spacing={5}  paddingLeft={2} marginBottom={5}>
-          <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})}/>
+        {/* <HStack spacing={5}  paddingLeft={2} marginBottom={5}> */}
+        <Flex paddingLeft={2} marginBottom={5}>
+          <Box marginRight={5}>
+            <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})}/>
+          </Box>
           <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})} />
-        </HStack>
+        </Flex>
+        {/* </HStack> */}
         {/* <GameGrid selectedPlatform={gameQuery.platform} selectedGenre={gameQuery.genre}/> */}
         <GameGrid gameQuery={gameQuery}/>
       </GridItem>
