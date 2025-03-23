cc9c3b90c68418c36cdc8731fe3b184173a0cc5a
Sagar G C <sagar@tulipstechnologies.com>
2025-03-23 10:26:00 +0545

    Clean Up the Game Cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -17,7 +17,7 @@ const GameCard = ( { game } : Props) => {
             <Image src={getCroppedImageUrl(game.background_image, 600, 400)} alt={game.name} />
             <CardBody>
                 <Heading fontSize='2xl'>{game.name}</Heading>
-                <HStack justifyContent='space-between'>
+                <HStack justifyContent='space-between' marginBottom={3}>
                     <PlatformIconList platforms={game.parent_platforms.map( p => p.platform )} />
                     <CriticScore score={game.metacritic} />
                 </HStack>
